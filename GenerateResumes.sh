dd10d357b3ad068c403374e56eb3a707cba68da2
Baigel <ahafeezbaig@gmail.com>
2026-03-22 08:47:06 +1000

    cleaning git hx

---
+++ GenerateResumes.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Script to generate multiple resumes from multiple *.company files
+# Not the most efficient way to do things 
+# (i.e should use `for %i in (*.company)` instead, 
+# so that only name of company used and '.company' 
+# part of name not included
+
+# Create child directory to hold resumes (if it doesn't already exist)
+mkdir -p ./Resumes
+
+# Loop through each file name
+for filename in *.company
+do
+    # Get just the name of the company (kinda hacky - regex would be better)
+    name="${filename%.company}"
+	
+    # Generate the pdf by importing the .company file
+    echo "LOG: Generating pdf for $name..."
+    pdflatex -jobname="$name" Resume_LaTeX.tex
+    # Try to concatinate academic_transcript.pdf onto cover letter/resume
+    echo "LOG: Concatenating academic_transcript.pdf"
+    pdftk "$name.pdf" academic_transcript.pdf cat output "$name2.pdf"
+    # If the concatenation was successfil then move the whole pdf to the 
+    # subdirectory and remove the original. If academic_transcript.pdf 
+    # doesn't exist, just move the pdf
+    if [ $? -ne 0 ]
+    then
+	echo "LOG: Failed to find academix transcipt, moving resume as is to Resumes folder..."
+        mv "$name.pdf" ./Resumes/"$name Cover Letter and Resume.pdf"
+    else
+	echo "LOG: Academic transcript added, moving resume to Resumes folder..."
+        mv "$name2.pdf" ./Resumes/"$name Cover Letter and Resume.pdf"
+        rm "$name.pdf"
+    fi
+done
---
+++ README.md
@@ -0,0 +1,10 @@
+# Resume-Generator
+
+A small script that generates multiple Resume's from a set of company-specific variables
+
+Given several `*.company` files generate a new pdf (in the `./Resumes` directory for each company. Although running the `GenerateResumes.sh` script works just fine, a Makefile has been included as the .DEFAULT_GOAL includes a cleanup process which gets rid of unneeded `*.log`, `*.aux` and `*.out` files that clog up the working directory.
+
+See [Example.company] file for basic outline. See "./Resumes/Example Cover Letter and Resume.pdf" for an example output.
+
+
+
